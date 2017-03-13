24850ae26c47b01f0dcd146a458e1205f55776d3
Yan Uehara <yanuehara@hotmail.com>
2017-03-13 13:29:02 -0400

    Corrigindo geração dos ids
    Signed-off-by: Yan Uehara <yanuehara@hotmail.com>

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ int main(int argc, char* argv[]){
     map<string, int> nametable;
 
     while(true){
-       
+
         int count = 0;
 
         std::pair<std::map<string, int>::iterator, bool> ret1;
@@ -41,7 +41,8 @@ int main(int argc, char* argv[]){
         //Lê a primeira linha
         cin >> num_vertices >> raiz;
         grafo = new Graph(num_vertices);
-        ret1 = nametable.insert(make_pair(raiz, count++));
+        ret1 = nametable.insert(make_pair(raiz, count));
+        count++;
 
         //Condição parada
         if(num_vertices == 0 && raiz == "FIM" )
@@ -52,8 +53,14 @@ int main(int argc, char* argv[]){
         for (int i = 0; i < total_vertices; i++) {
             cin >> especie1 >> especie2 >> distancia;
 
-            ret1 = nametable.insert(make_pair(especie1, count++));
-            ret2 = nametable.insert(make_pair(especie2, count++));
+            ret1 = nametable.insert(make_pair(especie1, count));
+            if(ret1.second){
+                count++;
+            }
+            ret2 = nametable.insert(make_pair(especie2, count));
+            if(ret2.second){
+                count++;
+            }
 
             grafo->add_adj(ret1.first->second, ret2.first->second, distancia);
         }
